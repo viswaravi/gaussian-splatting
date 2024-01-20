b78af213631c06e41ff490c2eaf1cd92a12faad0
viswa <rviswa00@gmail.com>
2024-01-20 20:33:18 +0100

    Added py train file - test from cluster

---
+++ train-progressive-synthetic.py
@@ -0,0 +1,18 @@
+import os
+import torch
+from random import randint
+from utils.loss_utils import l1_loss, ssim
+from gaussian_renderer import render, network_gui
+import sys
+from scene import Scene, GaussianModel
+from utils.general_utils import safe_state
+import uuid
+from tqdm import tqdm
+from utils.image_utils import psnr
+from argparse import ArgumentParser, Namespace
+from arguments import ModelParams, PipelineParams, OptimizationParams
+try:
+    from torch.utils.tensorboard import SummaryWriter
+    TENSORBOARD_FOUND = True
+except ImportError:
+    TENSORBOARD_FOUND = False
